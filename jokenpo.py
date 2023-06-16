742b8e92328b513d3e3ca0ffb54366015d1ce2cb
dboravitoria <dboravitoriasilva16@gmail.com>
2023-06-16 00:33:08 -0300

    inclusão da palavra jokenpô

--- jokenpo.py
+++ jokenpo.py
@@ -1,6 +1,7 @@
 #Crie um programa que faça o computador jogar Jokenpô com você. Pedra, Papel e Tesoura.
 #Use emojis representando uma mamão mostrando os símbolos.
 from random import choice
+from time import sleep 
 opcoes = [1, 2, 3]
 escolha = choice(opcoes)
 print('\033[1;31m=-=\033[m' * 25)
@@ -10,6 +11,9 @@ print('1 = Pedra\n2 = Papel\n3 = Tesoura')
 print('\033[1;31m=-=\033[m' * 25)
 jogada = int(input('Qual será a sua jogada: '))
 print('\033[1;31m=-=\033[m' * 25)
+print('JO', 'KEN', 'PÔ!!!')
+print('\033[1;31m=-=\033[m' * 25)
+sleep(1)
 if escolha == 1:
     print('A minha escolha foi {} que é Pedra'.format(escolha))
 elif escolha == 2:
@@ -20,11 +24,12 @@ print('\033[1;31m=-=\033[m' * 25)
 if jogada == escolha:
     print('\033[1;33mTente novamente, houve um empate!\033[m')
 elif (jogada == 1) and (escolha == 2):
-    print('Você perdeu, pois o papel cobre a pedra!')
+    print('Jogador perde, pois o papel cobre a pedra!')
 elif (jogada == 2) and (escolha == 3):
-    print('Você perdeu, pois a tesoura corta o papel!')
+    print('Jogador perde, pois a tesoura corta o papel!')
 elif (jogada == 3) and (escolha == 1):
-    print('Você perdeu, pois a pedra esmaga a tesoura!')
+    print('Jogador perde, pois a pedra esmaga a tesoura!')
 else:
     print('\033[1;34mParabéns você ganhou!\033[m')
 print('\033[1;31m=-=\033[m' * 25)
+
